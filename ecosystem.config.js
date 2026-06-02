51a10a35390772273e78e53355ff53b99fbf8835
Hammad Chuadhary <hammadchaudhary21@gmail.com>
2026-06-03 00:20:59 +0500

    Fix ecosystem.config.js cwd path for monorepo structure

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "sarj-worldwide",
       script: "node_modules/.bin/next",
       args: "start -p 3000",
-      cwd: "/var/www/sarjworldwide",
+      cwd: "/var/www/sarjworldwide/apps/web",
       instances: 1,
       autorestart: true,
       watch: false,
